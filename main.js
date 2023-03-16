44fe952be3b40785f5c38db260483684916f5ce5
Philiphogfeldt <philip.hogfeldteberdal@studerande.plushogskolan.se>
2023-03-16 15:27:45 +0100

    x

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 Vue.createApp({
     data() {
         return {
-            KEY: 'xOJnjrt9fhMMyvSANCOq9Cmrgiwa',
-            SECRET: 'KruYoiK1rDkPfJ_P44Qy_zdfxL4a',
-            ACCESS_TOKEN: '67effa0e-f71e-393b-9012-e5117f0de90b',
-            STOP_ID: '',
-            departures: [],
-            stopName: 'sjömarkenskolan',
+            key: 'xOJnjrt9fhMMyvSANCOq9Cmrgiwa',
+            secret: 'KruYoiK1rDkPfJ_P44Qy_zdfxL4a',
+            accessToken: '67effa0e-f71e-393b-9012-e5117f0de90b',
+            stopId: '',
+            departures: [[], [], []],
+            nextList: 0,
+            userInput: '',
         };
     },
-    created() {
-        this.getAccessToken()
-            .then(() => this.getStopId())
-            .then(() => this.getDepartures())
-    },
+    // created() {
+    //     this.getAccessToken()
+    //         .then(() => this.getStopId())
+    //         .then(() => this.getDepartures())
+    // },
     methods: {
         async getAccessToken() {
             const url = 'https://api.vasttrafik.se/token';
-            const headers = {
-                Authorization: `Basic ${btoa(`${this.KEY}:${this.SECRET}`)}`,
+            const callInfo = {
+                Authorization: `Basic ${btoa(`${this.key}:${this.secret}`)}`,
                 'Content-Type': 'application/x-www-form-urlencoded',
             };
             const body = new URLSearchParams({
@@ -28,67 +29,71 @@ Vue.createApp({
 
             const response = await fetch(url, {
                 method: 'POST',
-                headers,
+                headers: callInfo,
                 body,
             });
 
             const data = await response.json();
-            this.ACCESS_TOKEN = data.access_token;
+            this.accessToken = data.access_token;
         },
-        async getStopId(stopName) {
+        async getStopId() {
             const url = 'https://api.vasttrafik.se/bin/rest.exe/v2/location.name';
-            const headers = {
-                Authorization: `Bearer ${this.ACCESS_TOKEN}`,
+            const callInfo = {
+                Authorization: `Bearer ${this.accessToken}`,
             };
-            const params = new URLSearchParams({
+            const dataForSearch = new URLSearchParams({
                 format: 'json',
-                input: this.stopName,
+                input: this.userInput,
             });
 
-            const response = await fetch(`${url}?${params}`, {
-                headers,
+            const response = await fetch(`${url}?${dataForSearch}`, {
+                headers: callInfo,
             });
 
             const data = await response.json();
-            this.STOP_ID = data.LocationList.StopLocation[0].id;
+            this.stopId = data.LocationList.StopLocation[0].id;
         },
         async getDepartures() {
             await this.getAccessToken();
             await this.getStopId();
 
             const url = 'https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard';
-            const headers = {
-                Authorization: `Bearer ${this.ACCESS_TOKEN}`,
+            const callInfo = {
+                Authorization: `Bearer ${this.accessToken}`,
             };
             const now = new Date();
-            const params = new URLSearchParams({
+
+            const dataForSearch = new URLSearchParams({
                 format: 'json',
-                id: this.STOP_ID,
+                id: this.stopId,
                 date: `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`,
                 time: `${now.getHours()}:${now.getMinutes()}`,
             });
 
-            const response = await fetch(`${url}?${params}`, {
-                headers,
+            const response = await fetch(`${url}?${dataForSearch}`, {
+                headers: callInfo,
             });
 
             const data = await response.json();
             const serverDateTime = new Date(
                 `${data.DepartureBoard.serverdate} ${data.DepartureBoard.servertime}`
             );
-
-            this.departures = data.DepartureBoard.Departure
+            // if(this.nextList === 2){
+            //     this.nextList = 0;
+            // }
+            // else{}
+            this.departures[this.nextList] = data.DepartureBoard.Departure
                 .map((departure) => {
-                    const { sname, direction, time, date, rtTime, rtDate } = departure;
+                    const { sname, direction, time, date, realTime, realDate } = departure;
 
-                    const departureDateTime = rtTime
-                        ? new Date(`${rtDate} ${rtTime}`)
+                    const departureDateTime = realTime
+                        ? new Date(`${realDate} ${realTime}`)
                         : new Date(`${date} ${time}`);
 
                     const diff = Math.round(
                         (departureDateTime.getTime() - serverDateTime.getTime()) / 1000 / 60
                     );
-                    return { sname, direction, time, rtTime, diff };
+                    return { sname, direction, time, realTime, diff };
                 })
                 .filter((departure) => {
                     const departureDateTime = new Date(
@@ -98,10 +103,19 @@ Vue.createApp({
                 })
                 .sort((a, b) => a.diff - b.diff)
                 .slice(0, 5);
+                this.nextList++;
         },
         updateStop(stopName) {
             this.stopName = stopName;
             this.getDepartures();
         },
+        // checkLists(){
+        //     if (totalLists === 1){
+        //         this.departures2 === this.departures
+        //     }
+        //     else if (totalLists === 2){
+        //         this.departures3 === this.departures2
+        //     }
+        // }
     }
 }).mount('#app');
--- test2.js
+++ test2.js
@@ -1,140 +1,86 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>School and Back</title>
-  <style>
-    /* Add your styles here */
-  </style>
-</head>
-
-<body>
-  <h1>SwooshTrafik</h1>
-  <div id="app">
-    <h2>Avgångar från {{stopName}}</h2>
-    <form v-on:submit.prevent="updateStop">
-      <label for="stop-input">Skriv in en hållplats:</label>
-      <input type="text" id="stop-input" v-model="userInput">
-      <button type="submit">Uppdatera</button>
-    </form>
-    <div v-if="departures[0].length">
-      <h3>List 1:</h3>
-      <ul>
-        <li v-for="(departure, index) in departures[0]" :key="index">
-          {{departure.name}} avgår kl {{departure.departureTime}} om
-          {{departure.minutes}} minuter
-        </li>
-      </ul>
-    </div>
-    <div v-if="departures[1].length">
-      <h3>List 2:</h3>
-      <ul>
-        <li v-for="(departure, index) in departures[1]" :key="index">
-          {{departure.name}} avgår kl {{departure.departureTime}} om
-          {{departure.minutes}} minuter
-        </li>
-      </ul>
-    </div>
-    <div v-if="departures[2].length">
-      <h3>List 3:</h3>
-      <ul>
-        <li v-for="(departure, index) in departures[2]" :key="index">
-          {{departure.name}} avgår kl {{departure.departureTime}} om
-          {{departure.minutes}} minuter
-        </li>
-      </ul>
-    </div>
-  </div>
-
-  <script>
-    const app = {
-      data() {
-        return {
-          KEY: 'xOJnjrt9fhMMyvSANCOq9Cmrgiwa',
-          SECRET: 'KruYoiK1rDkPfJ_P44Qy_zdfxL4a',
-          ACCESS_TOKEN: '67effa0e-f71e-393b-9012-e5117f0de90b',
-          STOP_ID: '',
-          departures: [[], [], []],
-          currentListIndex: 0,
-          stopName: 'sjömarkenskolan',
-          userInput: ''
-        };
-      },
-      created() {
-        this.getAccessToken()
-          .then(() => this.getStopId())
-          .then(() => this.getDepartures());
-      },
-      methods: {
-        async getAccessToken() {
-          const url = 'https://api.vasttrafik.se/token';
-          const headers = {
-            Authorization: `Basic ${btoa(`${this.KEY}:${this.SECRET}`)}`,
-            'Content-Type': 'application/x-www-form-urlencoded',
-          };
-          const body = new URLSearchParams({
-            format: 'json',
-            grant_type: 'client_credentials',
-          });
-
-          const response = await fetch(url, {
-            method: 'POST',
-            headers,
-            body,
-          });
-
-          const data = await response.json();
-          this.ACCESS_TOKEN = data.access_token;
-        },
-        async getStopId() {
-            const url = `https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=${this.stopName}&format=json`;
-            const headers = {
-              Authorization: `Bearer ${this.ACCESS_TOKEN}`,
-            };
-      
-            const response = await fetch(url, {
-              headers,
-            });
-      
-            const data = await response.json();
-            const stopLocation = data.LocationList.StopLocation.find(stop => stop.name.toLowerCase() === this.stopName.toLowerCase());
-            this.STOP_ID = stopLocation.id;
-          },
-      
-          async getDepartures() {
-            const url = `https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=${this.STOP_ID}&date=${new Date().toISOString().slice(0, 10)}&time=${new Date().toISOString().slice(11, 16)}&format=json`;
-            const headers = {
-              Authorization: `Bearer ${this.ACCESS_TOKEN}`,
-            };
-      
-            const response = await fetch(url, {
-              headers,
-            });
-      
-            const data = await response.json();
-            const departures = data.DepartureBoard.Departure;
-      
-            departures.forEach(departure => {
-              const listIndex = this.currentListIndex % 3;
-              this.departures[listIndex].push({
-                name: departure.name,
-                departureTime: departure.time.slice(0, 5),
-                minutes: Math.round((new Date(`${new Date().toISOString().slice(0, 10)}T${departure.time}:00`) - new Date()) / 60000)
-              });
-              this.currentListIndex++;
-            });
-          },
-      
-          async updateStop() {
-            this.stopName = this.userInput.toLowerCase();
-            await this.getStopId();
-            await this.getDepartures();
-          },
-        },
+Vue.createApp({
+  data() {
+    return {
+      KEY: 'xOJnjrt9fhMMyvSANCOq9Cmrgiwa',
+      SECRET: 'KruYoiK1rDkPfJ_P44Qy_zdfxL4a',
+      ACCESS_TOKEN: '67effa0e-f71e-393b-9012-e5117f0de90b',
+      STOP_ID: '',
+      departures: [[], [], []],
+      currentListIndex: 0,
+      stopName: 'sjömarkenskolan',
+      userInput: ''
+    };
+  },
+  created() {
+    this.getAccessToken()
+      .then(() => this.getStopId())
+      .then(() => this.getDepartures());
+  },
+  methods: {
+    async getAccessToken() {
+      const url = 'https://api.vasttrafik.se/token';
+      const headers = {
+        Authorization: `Basic ${btoa(`${this.KEY}:${this.SECRET}`)}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
       };
-      
-      Vue.createApp(app).mount('#app');
-      
+      const body = new URLSearchParams({
+        format: 'json',
+        grant_type: 'client_credentials',
+      });
+
+      const response = await fetch(url, {
+        method: 'POST',
+        headers,
+        body,
+      });
+
+      const data = await response.json();
+      this.ACCESS_TOKEN = data.access_token;
+    },
+    async getStopId(stopName) {
+      const url = `https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=${this.stopName}&format=json`;
+      const headers = {
+        Authorization: `Bearer ${this.ACCESS_TOKEN}`,
+      };
+
+      const response = await fetch(url, {
+        headers,
+      });
+
+      const data = await response.json();
+      const stopLocation = data.LocationList.StopLocation.find(stop => stop.name.toLowerCase() === this.stopName.toLowerCase());
+      this.STOP_ID = stopLocation.id;
+    },
+
+    async getDepartures() {
+      const url = `https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=${this.STOP_ID}&date=${new Date().toISOString().slice(0, 10)}&time=${new Date().toISOString().slice(11, 16)}&format=json`;
+      const headers = {
+        Authorization: `Bearer ${this.ACCESS_TOKEN}`,
+      };
+
+      const response = await fetch(url, {
+        headers,
+      });
+
+      const data = await response.json();
+      const departures = data.DepartureBoard.Departure;
+
+      departures.forEach(departure => {
+        const listIndex = this.currentListIndex % 3;
+        this.departures[listIndex].push({
+          name: departure.name,
+          departureTime: departure.time.slice(0, 5),
+          minutes: Math.round((new Date(`${new Date().toISOString().slice(0, 10)}T${departure.time}:00`) - new Date()) / 60000)
+        });
+        this.currentListIndex++;
+      });
+    },
+
+    async updateStop() {
+      this.stopName = this.userInput.toLowerCase();
+      await this.getStopId();
+      await this.getDepartures();
+    },
+  },
+}).mount('#app');
+
